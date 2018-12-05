0950353238a684e3df1381e4b30b116a685c9c5b
Paulo Henrique Tokarski Glinski <paulotokarski@gmail.com>
2018-12-05 18:00:34 -0200

    Alteração nas rotas da API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: {
-    pedidos: 'https://fullstack-pedidos-api.herokuapp.com/pedidos',
-    produtos: 'https://fullstack-pedidos-api.herokuapp.com/produtos',
-    clientes: 'https://fullstack-pedidos-api.herokuapp.com/clientes'
+    pedidos: 'https://fullstack-pedidos.herokuapp.com/pedidos',
+    produtos: 'https://fullstack-pedidos.herokuapp.com/produtos',
+    clientes: 'https://fullstack-pedidos.herokuapp.com/clientes'
   }
 };
